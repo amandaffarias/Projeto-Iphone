08c5dc1db89a5a7c79e6cefc9238c6536f61cff8
amandaffarias <contactamandafarias@gmail.com>
2024-05-21 15:07:29 -0300

    primeiro-commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function changeColor(cor){
+    let circle = document.querySelector('.circle')
+ circle.style.background = cor
+}
+
+function changeIphone(celular){
+    let iphone = document.querySelector('.iphone')
+    iphone.src = celular
+}
